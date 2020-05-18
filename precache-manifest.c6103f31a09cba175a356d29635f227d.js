467a49f3bc6e055655273a7111ae71892d5a8eea
Ciro <cpontes@verdeghaia.com.br>
2020-05-17 22:07:43 -0300

    mobile responsive fixes

---
+++ precache-manifest.c6103f31a09cba175a356d29635f227d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf97075cf74e632379cf5eb926c8e19",
+    "url": "/FarejadorCNPJ/index.html"
+  },
+  {
+    "revision": "d5d0aed6c4880d928b39",
+    "url": "/FarejadorCNPJ/static/css/main.903fd986.chunk.css"
+  },
+  {
+    "revision": "1d77ac89273fc017817e",
+    "url": "/FarejadorCNPJ/static/js/0.375628e3.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/FarejadorCNPJ/static/js/0.375628e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ca9ada7f08c1de5421",
+    "url": "/FarejadorCNPJ/static/js/3.9acebe0a.chunk.js"
+  },
+  {
+    "revision": "54b9dd6c2b794df1aa69",
+    "url": "/FarejadorCNPJ/static/js/4.ec1a6ac5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FarejadorCNPJ/static/js/4.ec1a6ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f895a03217e407613d4",
+    "url": "/FarejadorCNPJ/static/js/5.2832b21b.chunk.js"
+  },
+  {
+    "revision": "689e59c88c2732fae10a",
+    "url": "/FarejadorCNPJ/static/js/6.ebf097a2.chunk.js"
+  },
+  {
+    "revision": "d5d0aed6c4880d928b39",
+    "url": "/FarejadorCNPJ/static/js/main.ca97f574.chunk.js"
+  },
+  {
+    "revision": "f21e7e41103e302db49f",
+    "url": "/FarejadorCNPJ/static/js/runtime-main.22c72f98.js"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/FarejadorCNPJ/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "3debcaf1a98e4445d213fd0089d42f6b",
+    "url": "/FarejadorCNPJ/static/media/fa-brands-400.3debcaf1.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/FarejadorCNPJ/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/FarejadorCNPJ/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/FarejadorCNPJ/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/FarejadorCNPJ/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89f9806b964f92ad282fd60947eba588",
+    "url": "/FarejadorCNPJ/static/media/fa-regular-400.89f9806b.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/FarejadorCNPJ/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/FarejadorCNPJ/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/FarejadorCNPJ/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/FarejadorCNPJ/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/FarejadorCNPJ/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "ad912fd102f4052d3273dd838e0c671d",
+    "url": "/FarejadorCNPJ/static/media/fa-solid-900.ad912fd1.svg"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/FarejadorCNPJ/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/FarejadorCNPJ/static/media/fa-solid-900.bea989e8.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6bcf4cae16f4283ffcc6d80618fb307d.js"
+  "/FarejadorCNPJ/precache-manifest.c6103f31a09cba175a356d29635f227d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FarejadorCNPJ/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
