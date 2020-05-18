aefd2bd4c7bc1bc5d32d87d94d7ae3085d3bf917
Ciro <cpontes@verdeghaia.com.br>
2020-05-17 22:16:24 -0300

    root folder fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FarejadorCNPJ/precache-manifest.c6103f31a09cba175a356d29635f227d.js"
+  "/precache-manifest.fb12da3ad6f6e752c72136661481c528.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FarejadorCNPJ/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
